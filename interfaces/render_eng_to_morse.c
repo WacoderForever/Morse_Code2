d18f231cf7d820f453ff29a8a399afa745dee56c
Wacoder Forever <sethomondi088@gmail.com>
2023-08-12 03:09:12 +0300

    f2

--- interfaces/render_eng_to_morse.c
+++ interfaces/render_eng_to_morse.c
@@ -1,31 +1,31 @@
 struct CTextStack *render_eng_to_morse(char *result){
     struct CTextStack *s=newCTextStack(CTEXT_LINE_BREAKER,CTEXT_SEPARATOR);
-    s->$open(s,CTEXT_HTML,"lang=\"en\"");
-        s->open(s,CTEXT_HEAD);
+    CTextStack_$open(s,CTEXT_HTML,"lang=\"en\"");
+        ctext_open(s,CTEXT_HEAD);
            create_basic_enntrys(s);
-            s->segment_text(s,"English to Morse");
-        s->close(s,CTEXT_HEAD);
+            CTextStack_segment_text(s,"English to Morse");
+        ctext_close(s,CTEXT_HEAD);
 
-        s->open(s,CTEXT_BODY);
-            create_link(s,"Morse to English","morse_to_eng");
-            s->auto$close(s,CTEXT_BR,"");
+        ctext_open(s,CTEXT_BODY);
+            create_link(s,"Morse to English","/morse_to_eng");
+            CTextStack_auto$close(s,CTEXT_BR,"");
             
-            s->$open(s,CTEXT_DIV,R"(class="center")");
-            s->open(s,CTEXT_H2);
-                s->segment_text(s,"English to Morse Converter");
-             s->close(s,CTEXT_H2);
+            CTextStack_$open(s,CTEXT_DIV,R"(class="center")");
+            ctext_open(s,CTEXT_H2);
+                CTextStack_segment_text(s,"English to Morse Converter");
+             ctext_close(s,CTEXT_H2);
 
-            s->$open(s,CTEXT_FORM,R"(action="/eng_to_morse" method="POST")")
-                s->auto$close(s,CTEXT_INPUT,R"(name="eng_text" placeholder="Enter English text")");
-                s->auto$close(s,CTEXT_BR,"");
+            CTextStack_$open(s,CTEXT_FORM,R"(action="/eng_to_morse" method="POST")");
+                CTextStack_auto$close(s,CTEXT_INPUT,R"(name="eng_text" placeholder="Enter English text")");
+                CTextStack_auto$close(s,CTEXT_BR,"");
                 create_button(s,"Convert");
-            s->close(s,CTEXT_FORM);
+            ctext_close(s,CTEXT_FORM);
         if(result !=NULL){
-            s->open(s,CTEXT_H3);
-                s->segment_text(s,result);
-            S->close(s,CTEXT_H3);
+            ctext_open(s,CTEXT_H3);
+                CTextStack_segment_text(s,result);
+            ctext_close(s,CTEXT_H3);
         }
-        s->close(s,CTEXT_BODY);
-    s->close(s,CTEXT_HTML);
+        ctext_close(s,CTEXT_BODY);
+    ctext_close(s,CTEXT_HTML);
         
 }
--- interfaces/render_morse_to_eng.c
+++ interfaces/render_morse_to_eng.c
@@ -1,30 +1,30 @@
 struct CTextStack *render_morse_to_eng(char *result){
     struct CTextStack *s=newCTextStack(CTEXT_LINE_BREAKER,CTEXT_SEPARATOR);
-    s->$open(s,CTEXT_HTML,"lang=\"en\"");
-        s->open(s,CTEXT_HEAD);
+     CTextStack_$open(s,CTEXT_HTML,"lang=\"en\"");
+        ctext_open(s,CTEXT_HEAD);
             create_basic_entryss(s);
-            s->segment_text(s,"Morse to English");
-        s->close(s,CTEXT_HEAD);
+            CTextStack_segment_text(s,"Morse to English");
+        ctext_close(s,CTEXT_HEAD);
 
-        s->open(s,CTEXT_BODY);
+        ctext_open(s,CTEXT_BODY);
             create_link(s,"English to morse","/eng_to_morse");
-            s->auto$close(s,CTEXT_BR,"");
-            s->$open(s,CTEXT_DIV,R"(class="center")");
-                s->open(s,CTEXT_H2);
-                    s->segment_text(s,"Morse to English Converter");
-                s->close(s,CTEXT_H2);
-                s->$open(s,CTEXT_FORM,R"(action="/morse_to_eng" method="POST")")
-                    s->$open(s,CTEXT_INPUT,R"(name="morse_text" placeholder="Enter morse text")");
-                    s->auto$close(s,CTEXT_BR,"");
+            CTextStack_auto$close(s,CTEXT_BR,"");
+            CTextStack_$open(s,CTEXT_DIV,R"(class="center")");
+                ctext_open(s,CTEXT_H2);
+                    CTextStack_segment_text(s,"Morse to English Converter");
+                ctext_close(s,CTEXT_H2);
+            CTextStack_$open(s,CTEXT_FORM,R"(action="/morse_to_eng" method="POST")");
+             CTextStack_$open(s,CTEXT_INPUT,R"(name="morse_text" placeholder="Enter morse text")");
+                CTextStack_auto$close(s,CTEXT_BR,"");
                     create_button(s,"Convert");
-                s->close(s,FORM);
+                ctext_close(s,CTEXT_FORM);
                 if(result != NULL){
-                    s->$open(s,CTEXT_H3);
-                        s->segment_text(s,result);
-                    s->close(s,CTEXT_H3);
+                ctext_open(s,CTEXT_H3);
+                        CTextStack_segment_text(s,result);
+                    ctext_close(s,CTEXT_H3);
                 }
                 
-            s->close(s,CTEXT_DIV);
-        s->close(s,CTEXT_BODY);
-    s->close(s,CTEXT_HTML);
+            ctext_close(s,CTEXT_DIV);
+        ctext_close(s,CTEXT_BODY);
+    ctext_close(s,CTEXT_HTML);
 }
